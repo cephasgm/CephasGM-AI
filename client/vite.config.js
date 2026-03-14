c0f98d245993fd0037ef22e75d0df8dda2ad3709
CephasGM <cephas.mkama@gmail.com>
2026-03-14 19:35:33 +0300

    Create vite.config.js

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:10000',
+      '/chat': 'http://localhost:10000',
+      '/generate': 'http://localhost:10000',
+      '/upload': 'http://localhost:10000',
+      '/task': 'http://localhost:10000',
+    }
+  }
+})
