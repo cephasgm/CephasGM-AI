7d8c44a4f8aa99821626377ac3f5d10498df6df5
CephasGM <cephas.mkama@gmail.com>
2026-03-14 20:08:14 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/app/', // <-- ADDED: ensures assets are served from /app
   plugins: [react()],
   server: {
     proxy: {
